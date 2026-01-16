cc64f5a6e0f9fb3c31da0b46a3b4137193d7034b
Sebastian Eydam <sebastian.eydam@cyberus-technology.de>
2026-01-16 11:42:40 +0100

    vmm: add mechanism for post-migration announcements
    Some devices can or should do announcements after a live migration,
    e.g., a network device announces its new location in a network to update
    the MAC->port mappings in layer 2 switches. With this change a device
    can return an announcement action ("announcer") to do that.
    
    We use reverse ARP (RARP) packets for this, similar to QEMU. Also, we
    do 5 rounds of announcements in background (in a dedicated thread).
    
    On-behalf-of: SAP sebastian.eydam@sap.com
    Signed-off-by: Sebastian Eydam <sebastian.eydam@cyberus-technology.de>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -3036,6 +3036,10 @@ impl Vm {
             .try_lock_disks()
             .map_err(Error::LockingError)?;
 
+        // TODO for upstreaming probably relevant
+        // Advertise new VM location to network switches.
+        // self.post_migration_announce();
+
         // Now we can start all vCPUs from here.
         self.cpu_manager
             .lock()
@@ -3272,6 +3276,14 @@ impl Vm {
             .unwrap()
             .nmi()
             .map_err(Error::ErrorNmi);
+    }
+
+    /// Calls [`DeviceManager::post_migration_announce`].
+    pub fn post_migration_announce(&self) {
+        self.device_manager
+            .lock()
+            .unwrap()
+            .post_migration_announce();
     }
 }
 
