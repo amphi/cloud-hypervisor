99a6e4938b4aee2512c78ceb8ca8c23ae12962b0
Sebastian Eydam <sebastian.eydam@cyberus-technology.de>
2026-03-12 14:30:43 +0100

    vmm: funnel VM memory via additional connections abstraction
    At this point, we are still only using a single connection.
    
    On-behalf-of: SAP sebastian.eydam@sap.com
    Signed-off-by: Sebastian Eydam <sebastian.eydam@cyberus-technology.de>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -6,6 +6,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Read, Write, stdout};
+use std::num::NonZero;
 use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
 use std::panic::AssertUnwindSafe;
 #[cfg(feature = "guest_debug")]
@@ -55,7 +56,9 @@ use crate::memory_manager::MemoryManager;
 #[cfg(all(feature = "kvm", target_arch = "x86_64"))]
 use crate::migration::get_vm_snapshot;
 use crate::migration::{recv_vm_config, recv_vm_state};
-use crate::migration_transport::SocketStream;
+use crate::migration_transport::{
+    ReceiveAdditionalConnections, ReceiveListener, SendAdditionalConnections, SocketStream,
+};
 use crate::seccomp_filters::{Thread, get_seccomp_filter};
 use crate::vm::{Error as VmError, Vm, VmState};
 use crate::vm_config::{
@@ -607,6 +610,13 @@ pub struct Vmm {
     console_info: Option<ConsoleInfo>,
 }
 
+/// Just a wrapper for the data that goes into
+/// [`ReceiveMigrationState::Configured`]
+struct ReceiveMigrationConfiguredData {
+    memory_manager: Arc<Mutex<MemoryManager>>,
+    guest_memory: GuestMemoryAtomic<GuestMemoryMmap>,
+    connections: ReceiveAdditionalConnections,
+}
 /// The receiver's state machine behind the migration protocol.
 enum ReceiveMigrationState {
     /// The connection is established and we haven't received any commands yet.
@@ -624,10 +634,7 @@ enum ReceiveMigrationState {
     ///
     /// We keep the memory manager around to pass it into the next state. From this point
     /// on, the sender can start sending memory updates.
-    Configured(
-        Arc<Mutex<MemoryManager>>,
-        GuestMemoryAtomic<GuestMemoryMmap>,
-    ),
+    Configured(ReceiveMigrationConfiguredData),
 
     /// Memory is populated and we received the state. The VM is ready to go.
     StateReceived,
@@ -827,6 +834,7 @@ impl Vmm {
     fn vm_receive_migration_step(
         &mut self,
         socket: &mut SocketStream,
+        listener: &ReceiveListener,
         state: ReceiveMigrationState,
         req: &Request,
         _receive_data_migration: &VmReceiveMigrationData,
@@ -839,19 +847,21 @@ impl Vmm {
             )))
         };
 
-        let mut configure_vm = |socket: &mut SocketStream,
-                                memory_files: HashMap<u32, File>|
-         -> std::result::Result<
-            (
-                Arc<Mutex<MemoryManager>>,
-                GuestMemoryAtomic<GuestMemoryMmap>,
-            ),
-            MigratableError,
-        > {
-            let memory_manager = self.vm_receive_config(req, socket, memory_files)?;
-            let guest_memory = memory_manager.lock().unwrap().guest_memory();
-            Ok((memory_manager, guest_memory))
-        };
+        let mut configure_vm =
+            |socket: &mut SocketStream,
+             memory_files: HashMap<u32, File>|
+             -> std::result::Result<ReceiveMigrationConfiguredData, MigratableError> {
+                let memory_manager = self.vm_receive_config(req, socket, memory_files)?;
+                let guest_memory = memory_manager.lock().unwrap().guest_memory();
+                let connections = listener
+                    .try_clone()
+                    .and_then(|l| ReceiveAdditionalConnections::new(l, guest_memory.clone()))?;
+                Ok(ReceiveMigrationConfiguredData {
+                    memory_manager,
+                    guest_memory,
+                    connections,
+                })
+            };
 
         let recv_memory_fd = |socket: &mut SocketStream,
                               mut memory_files: Vec<(u32, File)>|
@@ -874,28 +884,31 @@ impl Vmm {
             },
             Started => match req.command() {
                 Command::MemoryFd => recv_memory_fd(socket, Vec::new()).map(MemoryFdsReceived),
-                Command::Config => {
-                    configure_vm(socket, Default::default()).map(|res| Configured(res.0, res.1))
-                }
+                Command::Config => configure_vm(socket, Default::default()).map(Configured),
                 _ => invalid_command(),
             },
             MemoryFdsReceived(memory_files) => match req.command() {
                 Command::MemoryFd => recv_memory_fd(socket, memory_files).map(MemoryFdsReceived),
-                Command::Config => configure_vm(socket, HashMap::from_iter(memory_files))
-                    .map(|res| Configured(res.0, res.1)),
+                Command::Config => {
+                    configure_vm(socket, HashMap::from_iter(memory_files)).map(Configured)
+                }
                 _ => invalid_command(),
             },
-            Configured(memory_manager, guest_memory) => match req.command() {
+            Configured(mut config_data) => match req.command() {
                 Command::Memory => {
                     migration_transport::receive_memory_regions(
-                        &guest_memory,
+                        &config_data.guest_memory,
                         req,
                         socket,
-                    )?;
-                    Ok(Configured(memory_manager, guest_memory))
+                    )
+                    .inspect_err(|_| {
+                        config_data.connections.cleanup().ok();
+                    })?;
+                    Ok(Configured(config_data))
                 }
                 Command::State => {
-                    self.vm_receive_state(req, socket, memory_manager)?;
+                    config_data.connections.cleanup()?;
+                    self.vm_receive_state(req, socket, config_data.memory_manager)?;
                     Ok(StateReceived)
                 }
                 _ => invalid_command(),
@@ -1071,36 +1084,26 @@ impl Vmm {
 
     fn do_memory_migration(
         vm: &mut Vm,
-        mut socket: &mut SocketStream,
+        socket: &mut SocketStream,
+        mem_send: &mut SendAdditionalConnections,
     ) -> result::Result<(), MigratableError> {
-        migration_transport::send_memory_regions(
-            &vm.guest_memory(),
-            &vm.memory_range_table()?,
-            &mut socket,
-        )?;
+        mem_send.send_memory(&vm.memory_range_table()?, socket)?;
 
         // Try at most 5 passes of dirty memory sending
         const MAX_DIRTY_MIGRATIONS: usize = 5;
         for i in 0..MAX_DIRTY_MIGRATIONS {
             info!("Dirty memory migration {i} of {MAX_DIRTY_MIGRATIONS}");
             let table = vm.dirty_log()?;
-            if table.regions().is_empty() {
+            if !mem_send.send_memory(&table, socket)? {
                 break;
             }
-
-            migration_transport::send_memory_regions(&vm.guest_memory(), &table, &mut socket)?;
         }
 
         // Now pause VM
         vm.pause()?;
 
         // Send last batch of dirty pages
-        migration_transport::send_memory_regions(
-            &vm.guest_memory(),
-            &vm.dirty_log()?,
-            &mut socket,
-        )?;
-
+        mem_send.send_memory(&vm.dirty_log()?, socket)?;
         Ok(())
     }
 
@@ -1179,9 +1182,19 @@ impl Vmm {
             // Now pause VM
             vm.pause()?;
         } else {
+            let mut mem_send = migration_transport::SendAdditionalConnections::new(
+                &send_data_migration.destination_url,
+                NonZero::new(1).unwrap(),
+                &vm.guest_memory(),
+            )?;
             // Start logging dirty pages
             vm.start_dirty_log()?;
-            Self::do_memory_migration(vm, &mut socket)?;
+            Self::do_memory_migration(vm, &mut socket, &mut mem_send).inspect_err(|_| {
+                // Calling cleanup multiple times is fine, thus here we just make sure
+                // that it is called.
+                mem_send.cleanup().ok();
+            })?;
+            mem_send.cleanup()?;
         }
 
         // We release the locks early to enable locking them on the destination host.
@@ -2136,6 +2149,7 @@ impl RequestHandler for Vmm {
 
             let (response, new_state) = match self.vm_receive_migration_step(
                 &mut socket,
+                &listener,
                 state,
                 &req,
                 &receive_data_migration,
